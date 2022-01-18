c8333451589eab99b4f504a01b7545ada44f28bc
Takashi Nemoto <tnemoto@eels.kuicr.kyoto-u.ac.jp>
2022-01-18 12:48:42 +0900

    Enhancement of markers 1. zorder property 2. filled rectangle / text box 3. new marker : arrow, ellipse

--- hyperspy/drawing/_markers/ellipse.py
+++ hyperspy/drawing/_markers/ellipse.py
@@ -16,27 +16,30 @@
 # You should have received a copy of the GNU General Public License
 # along with  HyperSpy.  If not, see <http://www.gnu.org/licenses/>.
 
-import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 from hyperspy.drawing.marker import MarkerBase
 
 
-class VerticalLineSegment(MarkerBase):
+class Ellipse(MarkerBase):
 
-    """Vertical line segment marker that can be added to the signal figure
+    """Rectangle marker that can be added to the signal figure
 
     Parameters
     ----------
-    x : array or float
-        The position of line segment in x.
+    x1 : array or float
+        The position of the up left corner of the rectangle in x.
         If float, the marker is fixed.
         If array, the marker will be updated when navigating. The array should
         have the same dimensions in the navigation axes.
     y1 : array or float
-        The position of the start of the line segment in x.
+        The position of the up left corner of the rectangle in y.
+        see x1 arguments
+    x2 : array or float
+        The position of the down right corner of the rectangle in x.
         see x1 arguments
     y2 : array or float
-        The position of the start of the line segment in y.
+        The position of the down right of the rectangle in y.
         see x1 arguments
     kwargs :
         Keywords argument of axvline valid properties (i.e. recognized by
@@ -44,35 +47,43 @@ class VerticalLineSegment(MarkerBase):
 
     Example
     -------
-    >>> im = hs.signals.Signal2D(np.zeros((100, 100)))
-    >>> m = hs.plot.markers.vertical_line_segment(
-    >>>     x=20, y1=30, y2=70, linewidth=4, color='red', linestyle='dotted')
+    >>> import scipy.misc
+    >>> im = hs.signals.Signal2D(scipy.misc.ascent())
+    >>> m = hs.plot.markers.rectangle(x1=150, y1=100, x2=400, y2=400,
+    >>>                                  color='red')
     >>> im.add_marker(m)
 
-    Add a marker permanently to a marker
+    Adding a marker permanently to a signal
 
-    >>> im = hs.signals.Signal2D(np.zeros((60, 60)))
-    >>> m = hs.plot.markers.vertical_line_segment(x=10, y1=20, y2=50)
+    >>> im = hs.signals.Signal2D(np.random.random((50, 50))
+    >>> m = hs.plot.markers.rectangle(x1=20, y1=30, x2=40, y2=49)
     >>> im.add_marker(m, permanent=True)
     """
 
-    def __init__(self, x, y1, y2, **kwargs):
+    def __init__(self, x1, y1, x2, y2, **kwargs):
         MarkerBase.__init__(self)
-        lp = {'color': 'black', 'linewidth': 1, 'zorder': None}
+        lp = {'edgecolor': 'black', 'facecolor': None, 'fill': None,
+              'linewidth': 1, 'zorder' : None}
         self.marker_properties = lp
-        self.set_data(x1=x, y1=y1, y2=y2)
+        self.set_data(x1=x1, y1=y1, x2=x2, y2=y2)
         self.set_marker_properties(**kwargs)
-        self.name = 'vertical_line_segment'
+        mp = self.marker_properties
+        if 'color' in mp:
+            mp['edgecolor'] = mp['color']
+            del mp['color']
+        self.name = 'ellipse'
 
     def __repr__(self):
-        string = "<marker.{}, {} (x={},y1={},y2={},linewidth={},color={},zorder={})>".format(
+        string = "<marker.{}, {} (x1={},x2={},y1={},y2={},edgecolor={},facecolor={},linewidth={},zorder={})>".format(
             self.__class__.__name__,
             self.name,
             self.get_data_position('x1'),
+            self.get_data_position('x2'),
             self.get_data_position('y1'),
             self.get_data_position('y2'),
+            self.marker_properties['edgecolor'],
+            self.marker_properties['facecolor'],
             self.marker_properties['linewidth'],
-            self.marker_properties['color'],
             self.marker_properties['zorder'],
         )
         return(string)
@@ -80,22 +91,13 @@ class VerticalLineSegment(MarkerBase):
     def update(self):
         if self.auto_update is False:
             return
-        self._update_segment()
+        xy, _, width, height = self.get_xywh()
+        self.marker.set_xy(xy)
+        self.marker.set_width(width)
+        self.marker.set_height(height)
 
     def _plot_marker(self):
-        self.marker = self.ax.vlines(0, 0, 1, **self.marker_properties)
-        self._update_segment()
+        xy, _, width, height = self.get_xywh()
+        self.marker = self.ax.add_patch(patches.Ellipse(
+            xy, width, height, **self.marker_properties))
 
-    def _update_segment(self):
-        segments = self.marker.get_segments()
-        segments[0][0, 0] = self.get_data_position('x1')
-        segments[0][1, 0] = segments[0][0, 0]
-        if self.get_data_position('y1') is None:
-            segments[0][0, 1] = plt.getp(self.marker.axes, 'ylim')[0]
-        else:
-            segments[0][0, 1] = self.get_data_position('y1')
-        if self.get_data_position('y2') is None:
-            segments[0][1, 1] = plt.getp(self.marker.axes, 'ylim')[1]
-        else:
-            segments[0][1, 1] = self.get_data_position('y2')
-        self.marker.set_segments(segments)
